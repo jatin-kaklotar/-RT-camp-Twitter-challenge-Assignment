b58fc086cb5535a1a6d9e6b2863bc723d2c7dea0
jatin-kaklotar <jatin_kaklotar_mscit@yahoo.com>
2017-07-17 11:01:48 +0530

    Update csv_try.php

--- csv_try.php
+++ csv_try.php
@@ -1,18 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jatin
- * Date: 8/6/17
- * Time: 10:53 PM
- */
 
 session_start();
-
 $menu=$_SESSION['my'];
 $data=array();
 foreach ($menu as $key=>$value)
 {
-
     $data[$key]['number']=$key+1;
     $data[$key]['tweet']=$value[1];
 }
@@ -25,9 +17,7 @@ function cleanData(&$str)
     if(strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
 }
 
-// filename for download
 $filename = "my_tweets_" . date('Ymdhis') . ".csv";
-
 header("Content-Disposition: attachment; filename=\"$filename\"");
 header("Content-Type: application/csv");
 
@@ -42,26 +32,5 @@ foreach($data as $row) {
     echo implode("\t", array_values($row)) . "\r\n";
 }
 
-die;
-
-
-function convert_to_csv($input_array, $output_file_name, $delimiter)
-{
-    $temp_memory = fopen('php://memory', 'w');
-    foreach ($input_array as $line) {
-        fputcsv($temp_memory, $line, $delimiter);
-    }
-
-    fseek($temp_memory, 0);
-   // modify the header to be CSV format
-    header('Content-Type: application/csv');
-    header('Content-Disposition: attachement; filename="' . $output_file_name . '";');
-    fpassthru($temp_memory);
-}
-
-$title_csv = date("Ymdhis");
-
-
-convert_to_csv($menu, $title_csv.'.csv', ',');
 
 
