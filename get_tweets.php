2e475c619cdeade228e1bf8acfe1ce6ad7d0dc03
jatin-kaklotar <jatin_kaklotar_mscit@yahoo.com>
2017-07-23 15:03:10 +0530

    Update get_tweets.php

--- get_tweets.php
+++ get_tweets.php
@@ -1,19 +1,14 @@
 <?php
-
 session_start();
 require 'autoload.php';
-//require "vendor/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
-
 define('CONSUMER_KEY', 'aeKsT8Hlp7kg0i91SLXK4NB6o'); // add your app consumer key between single quotes
 define('CONSUMER_SECRET', 'QRbDVwhbbJk6CokKdQN3dHjwSzve45V73uH24ZalLaQIM2QDuL'); // add your app consumer secret key between single quotes
 define('OAUTH_CALLBACK', 'https://jatin-kaklotar.000webhostapp.com/twitter_demo/callback.php'); // your app callback URL
 
-
 if (!isset($_SESSION['access_token'])) {
     header('Location: ./');
 } else {
-
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 
@@ -31,27 +26,26 @@ if (!isset($_SESSION['access_token'])) {
                 $my_array[$key]['images'] = '';
             }
         }
-
     } else {
-
         $followerstweet = $connection->get("statuses/home_timeline", array('count' => 10));
         $my_array = array();
-        foreach ($followerstweet as $key => $ftweet) {
-
-            $string_text =$ftweet->text;
-            $my_array[$key]['text']=preg_replace("~[[:alpha:]]+://[^<>[:space:]]+[[:alnum:]/]~","<a target='_blank' href=\"\\0\">\\0</a>",$string_text);
-            if (isset($ftweet->entities->media[0]->media_url)) {
-                $image_f = '<img src="' . $ftweet->entities->media[0]->media_url . '" height="150" width="150" /> &nbsp;';
-                $my_array[$key]['images'] = $image_f;
-            } else {
-                $my_array[$key]['images'] = '';
+        if(isset($followerstweet->errors[0]->code)) {
+            $my_array[0]['text']  = "Rate Limit exceesed Please refresh page after some minutes";
+            $my_array[0]['images'] = '';
+        }
+        else {
+            foreach ($followerstweet as $key => $ftweet) {
+                $string_text = $ftweet->text;
+                $my_array[$key]['text'] = preg_replace("~[[:alpha:]]+://[^<>[:space:]]+[[:alnum:]/]~", "<a target='_blank' href=\"\\0\">\\0</a>", $string_text);
+                if (isset($ftweet->entities->media[0]->media_url)) {
+                    $image_f = '<img src="' . $ftweet->entities->media[0]->media_url_https .'" height="150" width="150" /> &nbsp;';
+                    $my_array[$key]['images'] = $image_f;
+                } else {
+                    $my_array[$key]['images'] = '';
+                }
             }
-
         }
     }
-
     echo json_encode(array('success' => 'true', 'display_array' => $my_array));
-
 }
-
 ?>
