710251b343f893781aa50b482aea9a86ca048cad
jatin-kaklotar <jatin_kaklotar_mscit@yahoo.com>
2017-07-23 15:16:14 +0530

    Update callback.php

--- callback.php
+++ callback.php
@@ -2,6 +2,9 @@
 session_start();
 require 'autoload.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
+define('CONSUMER_KEY', 'aeKsT8Hlp7kg0i91SLXK4NB6o'); // add your app consumer key between single quotes
+define('CONSUMER_SECRET', 'QRbDVwhbbJk6CokKdQN3dHjwSzve45V73uH24ZalLaQIM2QDuL'); // add your app consumer secret key between single quotes
+define('OAUTH_CALLBACK', 'https://jatin-kaklotar.000webhostapp.com/twitter_demo/callback.php'); // your app callback URL
 if (isset($_REQUEST['oauth_verifier'], $_REQUEST['oauth_token']) && $_REQUEST['oauth_token'] == $_SESSION['oauth_token']) {
 	$request_token = [];
 	$request_token['oauth_token'] = $_SESSION['oauth_token'];
@@ -9,6 +12,5 @@ if (isset($_REQUEST['oauth_verifier'], $_REQUEST['oauth_token']) && $_REQUEST['o
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
 	$access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_REQUEST['oauth_verifier']));
 	$_SESSION['access_token'] = $access_token;
-	// redirect user back to index page
 	header('Location: ./');
 }
