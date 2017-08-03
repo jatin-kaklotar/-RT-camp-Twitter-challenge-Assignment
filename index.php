f23fc01e5551afde4a328b2ee4ce39ed4e37b2f1
jatin-kaklotar <jatin_kaklotar_mscit@yahoo.com>
2017-08-03 17:47:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="lib/css/style.css">
     <script src="lib/js/index.js"></script>
+    <script src="lib/js/twitter_js.js"></script>
 </head>
 <body>
 <?php
@@ -143,139 +144,8 @@ if(isset($_GET['linkdrive']))
         $('#myModal1').modal('show');
         $("#linkinfo").prop("href", link_drive);
     </script>
-
 <?php } ?>  
-<script>
-    $(document).ready(function () {
-        $('.btnfollowertwitt').click(function () {
-            var followerID = $(this).val();
-            $.ajax({
-                type: 'get',
-                url: 'get_tweets.php', 
-                data: 'followerID=' + followerID, 
-                dataType: 'json',
-                success: function (result) {
-                    var htmld = "";
-                    $.each(result['display_array'], function (index, data) {
-                        htmld += '<li>' + data["text"] + '<br>' + data["images"] + '</li>';
-                    });
-                    var tweet_lenth = result['display_array'].length;
-                    if (tweet_lenth == 1) {
-                        htmld += '<li></li>';
-                    }
-                    else if (tweet_lenth == 0) {
-                        htmld += '<li></li><li>No tweets found</li>';
-                    }
-                    $("#div66").html(htmld);
-                }
-            });
-
-        });
-
-        $.ajax({
-            type: 'get',
-            url: 'get_tweets.php',
-            data: {followerID: null}, 
-            dataType: 'json',
-            success: function (result) {
-                var htmld = "";
-                $.each(result['display_array'], function (index, data) {
-                    htmld += '<li>' + data["text"] + '<br>' + data["images"] + '</li>';
-                });
-                var tweet_lenth = result['display_array'].length;
-                if (tweet_lenth == 1) {
-                    htmld += '<li></li>';
-                }
-                else if (tweet_lenth == 0) {
-                    htmld += '<li></li><li>No tweets found</li>';
-                }
-                $("#div66").html(htmld);
-            }
-        });
-
-    });
-
-    $(document).ready(function () {
-        $("#search-box").keyup(function () {
-            var timer; 
-            if ($(this).val() != '') {
-                $.ajax({
-                    type: "POST",
-                    url: "search_followers.php",
-                    data: 'keyword=' + $(this).val(),
-                    success: function (data) {
-                        $("#suggesstion-box").show();
-                        $("#suggesstion-box").html(data);
-                        $("#search-box").css("background", "#FFF");
-                    },
-                    beforeSend: function () {            
-                        timer = setTimeout(function()
-                            {
-                                $("#msg-box").show();
-                            },
-                            3000);
-                    },
-                    complete: function () {
-                        clearTimeout(timer);
-                        $("#msg-box").hide();
-                    }
-                });
-            }
-            else {
-                $("#suggesstion-box").empty();
-            }
-        });
-    });
-
-
-    function selectFollowers(val) {
-        $("#search-box").val(val);
-        $.ajax({
-            type: 'get',
-            url: 'get_tweets.php', 
-            data: 'followerID=' + val, 
-            dataType: 'json',
-            success: function (result) {
-                var htmld = "";
-                if (result['display_array'] == '') {
-
-                    htmld += '<li></li><li>No tweets found</li>';
-                }
-                else {
-                    var tweet_lenth = result['display_array'].length;
-                    if (tweet_lenth == 1) {
-                        htmld += '<li></li>';
-                    }
-                    $.each(result['display_array'], function (index, data) {
-                        htmld += '<li>' + data["text"] + '<br>' + data["images"] + '</li>';
-                    });
-                }
-                $("#div66").html(htmld);
-            }
-        });
-        $("#suggesstion-box").hide();
-    }
-
-    
-    $('.download_tweet').click(function () {
-        $(".download_select").prop("disabled", true);
-        $(".download_msg").show();
-        $.ajax({
-            type: 'get',
-            url: 'my_tweets.php', //Here you will fetch records
-            dataType: 'json',
-            success: function (result) {
-                if (result == 1) {
-                    $(".download_select").prop("disabled", false);
-                    $(".download_msg").hide();
-
-                }else {
-                    alert("something went wrong please try again");
-                }
-            }
-        });
-    });
-</script><br><br>
+<br><br>
 <div class="container-fluid">
     <div class="col-md-10 col-md-offset-2 col-sm-12" >
         <div class="frmSearch col-md-9 col-md-offset-3 col-sm-12" style="padding-left: 43px">
@@ -291,14 +161,14 @@ if(isset($_GET['linkdrive']))
             echo "<div class='col-md-10 col-md-offset-2 col-sm-12'>Rate Limit exceesed Please refesh page after some minutes</div>";
         }else {
           foreach ($followerslist->users as $follwers_random) {
-            echo "<div class='col-md-6  col-sm-12'  style='padding-bottom:40px;'>";
-            echo "<img src='$follwers_random->profile_image_url_https'>";
-            echo "<button class='btnfollowertwitt btn btn btn-lg' value='$follwers_random->screen_name'>$follwers_random->name</button>";
-            echo "</div>";
-            }
-        }
-         }
-        ?>
+                    echo "<div class='col-md-6  col-sm-12'  style='padding-bottom:40px;'>";
+                    echo "<img src='$follwers_random->profile_image_url_https'>";
+                    echo "<button class='btnfollowertwitt btn btn btn-lg' value='$follwers_random->screen_name'>$follwers_random->name</button>";
+                    echo "</div>"; 
+                 }
+              }
+}
+?>
         </div>
     </div>
 </div>
